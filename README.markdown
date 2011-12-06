94b876884160c5cd5079a5ce4fa0141e93ebab96
mdoff <mode.off@gmail.com>
2011-12-06 19:10:27 +0100

    initial

---
+++ README.markdown
@@ -0,0 +1,6 @@
+#Example of using arrays in portesql
+
+Just simple example of using arrays in tables on portesql
+Most important thing is use a gem called [activerecord-postgres-array](https://github.com/funny-falcon/activerecord-postgresql-arrays)
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'a494844834ef4f221d7dc59944d0679d2cfc43d66f5922fdd5e3187142a910236dd6192fd31cbd2a5fd2bf70ef782c31838a26d8aa4241f58d0cca4eb96cbb8e'
